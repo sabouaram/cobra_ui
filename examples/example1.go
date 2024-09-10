b3422056ba50c00ec36651f986923da62c6bed75
Salim Amine Bou Aram <salimamine.bouaram.ext@orange.com>
2024-09-10 20:54:21 +0200

    adding examples

---
+++ examples/example1.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+	cobra_ui "github.com/sabouaram/cobra-ui"
+)
+
+func main() {
+	var selectedFile string
+	ui := cobra_ui.New()
+	ui.SetQuestions([]cobra_ui.Question{
+		{
+			Text:     "Select a file:",
+			Color:    color.FgCyan,
+			FilePath: true,
+			Handler: func(filePath string) error {
+				selectedFile = filePath
+				return nil
+			},
+		},
+	})
+	ui.RunInteractiveUI()
+	fmt.Printf("Selected file full path: %s\n", selectedFile)
+}
