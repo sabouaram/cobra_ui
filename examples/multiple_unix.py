0cac369e9fbc3c1ed629ea5d86857cfe1528054f
Salim-Amine BOU ARAM <60610171+sabouaram@users.noreply.github.com>
2024-09-20 23:56:15 +0200

    Update multiple_unix.py

--- examples/multiple_unix.py
+++ examples/multiple_unix.py
@@ -1,1 +1,19 @@
+import pexpect
 
+def run_test():
+  
+    child = pexpect.spawn('go run example2.go')
+
+    # Wait for the prompt
+    child.expect(pexpect.TIMEOUT, timeout=10)
+
+    child.send('\x1b[B3')  
+    child.send('\r')     
+
+   
+    child.expect('Selected choice: Python')
+  
+    child.close()
+
+if __name__ == "__main__":
+    run_test()
