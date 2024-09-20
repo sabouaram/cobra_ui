fdbda5e14ada689a92bcca6cccd6157349fa4768
Salim-Amine BOU ARAM <60610171+sabouaram@users.noreply.github.com>
2024-09-20 20:51:48 +0200

    Create file_unix.py

---
+++ file_unix.py
@@ -0,0 +1,19 @@
+import pexpect
+
+def run_test():
+  
+    child = pexpect.spawn('go run examples/example1.go')
+
+    # Wait for the prompt
+    child.expect('Select a file:')
+
+    child.send('/tmp/test_dir/file1.txt')  
+    child.send('\r')      # Enter key
+
+   
+    child.expect('Selected file: file1.txt')
+  
+    child.close()
+
+if __name__ == "__main__":
+    run_test()
