6d5ac442d2a076e39938c1af08049b721a4d5843
Salim-Amine BOU ARAM <60610171+sabouaram@users.noreply.github.com>
2024-09-20 23:59:37 +0200

    Update pwd_windows.py

--- examples/pwd_windows.py
+++ examples/pwd_windows.py
@@ -1,1 +1,19 @@
+import wexpect
 
+def run_test():
+  
+    child = wexpect.spawn('go run example4.go')
+
+    # Wait for the prompt
+    child.expect('Enter your password: ')
+
+    child.send('123456')  
+    child.send('\r')      # Enter key
+
+   
+    child.expect('Password entered => 123456')
+  
+    child.close()
+
+if __name__ == "__main__":
+    run_test()
