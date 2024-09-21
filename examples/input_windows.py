0ed009e0de7be307054309dceeb3c2cf14f9dd1e
Salim-Amine BOU ARAM <60610171+sabouaram@users.noreply.github.com>
2024-09-21 03:00:21 +0200

    Update input_windows.py

--- examples/input_windows.py
+++ examples/input_windows.py
@@ -3,14 +3,18 @@ import wexpect
 def run_test():
 
     command = r'D:\a\cobra_ui\cobra_ui\examples\example3.exe'
+
     
-    child = wexpect.spawn(command + ' > output.log')
+    
+    child = wexpect.spawn(command)
 
     child.expect('Enter your age: ',timeout=5)
 
-    print(child.before)
 
     child.send('25\n\r')
+
+    child.expect('')
+    print(child.before)
 
     child.close()
 
