49a583cc0f6f28dfc027b40f64982b713afdd658
Salim-Amine BOU ARAM <60610171+sabouaram@users.noreply.github.com>
2024-09-21 02:36:38 +0200

    Update multiple_windows.py

--- examples/multiple_windows.py
+++ examples/multiple_windows.py
@@ -3,7 +3,8 @@ import wexpect
 def run_test():
   
     command = r'D:\a\cobra_ui\cobra_ui\examples\example2.exe'
-    child = wexpect.spawn(command)
+    child = wexpect.spawn(command + ' > output.log')
+
     # Wait for the prompt
     child.expect(pexpect.TIMEOUT, timeout=10)
 
